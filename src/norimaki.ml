e5f1b14c93146a7b59ac8b60db9bad92046bf00a
Gamicy <cjh14133@gmail.com>
2020-11-18 22:28:01 +0900

    sixth commit

--- src/norimaki.ml
+++ src/norimaki.ml
@@ -1,4 +1,12 @@
 let csv = Csv.load "tecl.csv" in
 let array = Csv.to_array csv in
 let cell = array.(1).(1) in
-Printf.printf "%s" cell
+Printf.printf "%s\n" cell ;
+let a = Array.make 10 0.0 in
+for i = 0 to 9 do 
+Printf.printf "i=%d:%s\n" i array.(i+2).(1) ;
+Printf.printf "i=%d:%s\n" i array.(i+1).(1) ;
+a.(i) <- (float_of_string(array.(i+2).(1))  -. float_of_string(array.(i+1).(1)) ) 
+done ;
+let cella = a.(0) in
+Printf.printf "%f" cella ;
